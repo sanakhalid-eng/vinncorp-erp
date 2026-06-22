002b563d4d06c1ffbf7a00791027f3bb0bd760be
Sana Khalid <sanakhalid@vinncorp.com>
2026-06-22 16:06:40 +0500

    fix: update test imports for WorkflowStatus and add TestJwtKeyProvider

--- backend/src/test/java/com/vinncorp/erp/AbstractIntegrationTest.java
+++ backend/src/test/java/com/vinncorp/erp/AbstractIntegrationTest.java
@@ -19,7 +19,7 @@ import com.vinncorp.erp.platform.workspace.service.CurrentWorkspaceResolver;
 import com.vinncorp.erp.platform.workspace.service.impl.DefaultWorkspaceResolver;
 import com.vinncorp.erp.modules.projects.controller.SystemController;
 import com.vinncorp.erp.modules.projects.entity.*;
-import com.vinncorp.erp.modules.workflow.entity.WorkflowStatus;
+import com.vinncorp.erp.modules.projects.entity.WorkflowStatus;
 import com.vinncorp.erp.modules.workflow.repository.WorkflowStatusRepository;
 import com.vinncorp.erp.platform.notification.repository.NotificationRepository;
 import com.vinncorp.erp.modules.projects.enums.RoleScope;
